82c6baa6066dd1d00a8bd0bb8c740db39a0ec580
KidSysco <kidsysco@gmail.com>
2025-01-03 14:51:56 -0600

    remove unused code

--- src/typed-router.d.ts
+++ src/typed-router.d.ts
@@ -20,6 +20,7 @@ declare module 'vue-router/auto-routes' {
   export interface RouteNamedMap {
     '/': RouteRecordInfo<'/', '/', Record<never, never>, Record<never, never>>,
     '/[...path]': RouteRecordInfo<'/[...path]', '/:path(.*)', { path: ParamValue<true> }, { path: ParamValue<false> }>,
+    '/Details': RouteRecordInfo<'/Details', '/Details', Record<never, never>, Record<never, never>>,
     '/nftMetaDetails/evmMetaDataGalleryDetails': RouteRecordInfo<'/nftMetaDetails/evmMetaDataGalleryDetails', '/nftMetaDetails/evmMetaDataGalleryDetails', Record<never, never>, Record<never, never>>,
     '/nftMetaDetails/evmNftGalleryDetails': RouteRecordInfo<'/nftMetaDetails/evmNftGalleryDetails', '/nftMetaDetails/evmNftGalleryDetails', Record<never, never>, Record<never, never>>,
     '/useEvmMetaDataGallery': RouteRecordInfo<'/useEvmMetaDataGallery', '/useEvmMetaDataGallery', Record<never, never>, Record<never, never>>,
