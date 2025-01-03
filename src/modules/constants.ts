0c50bbff438a9b39087eb5f4cfa37aa569d968ca
KidSysco <kidsysco@gmail.com>
2025-01-03 14:52:12 -0600

    add site title constant

--- src/modules/constants.ts
+++ src/modules/constants.ts
@@ -1,3 +1,5 @@
+export const SITE_TITLE = 'Vuetify NFT Gallery (TS)';
+
 // Base64-encoded transparent 1x1 PNG image.
 export const LAZY_SRC_PLACEHOLDER =
-  "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mP8/wkAArwB9vHff9kAAAAASUVORK5CYII=";
+  'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mP8/wkAArwB9vHff9kAAAAASUVORK5CYII=';
